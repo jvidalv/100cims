cff1dc8400cf77f78109edd520cb700efdac0344
jvidalv <josepvidalvidal@gmail.com>
2024-12-01 14:24:40 +0100

    feat: setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.tsx"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
